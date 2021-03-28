6fc257ec6e9545f415b160b2df603dcc5bd10099
Mateo Sam <sammateo4@gmail.com>
2021-03-28 15:54:59 -0400

    Gallery Formatting

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,5 @@
 import '../styles/globals.css'
-
+import '../styles/gallery.css'
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
--- pages/gallery.js
+++ pages/gallery.js
@@ -3,25 +3,35 @@ import styles from '../styles/gallery.module.css'
 import Image from 'next/image'
 export default function Gallery(){
 
-    let imgarray = ["/logo.jpeg","/roti.jpg"]
+    let imgarray = ["/logo.jpeg","/roti.jpg","/roll.jpg","/gallery1.jpg","/gallery2.jpg","/gallery3.jpg",
+                    "/gallery4.jpg","/gallery5.jpg","/gallery6.jpg","/gallery7.jpg","/gallery8.jpg",
+                    "/gallery9.jpg","/gallery10.jpg","/gallery11.jpg","/gallery12.jpg"
+                    ]
     return(
         <div className={styles.container}>
             <h1>Gallery</h1>
             <div className={styles.gallery}>
             {imgarray.map((img,id)=>(
                     <div className={styles.logo}>
-                    <Image
+                    <Link href={img}>
+                    <img
                       src={img}
                       alt="galleryimg"
                       width={300}
                       height={300}
                       />
+                    </Link>    
+                    
                 </div>
             ))}
             </div>
-            <Link href="/">
-                <button className={styles.backButton}>Back to Home</button>
-            </Link>
+
+            <div>
+                <Link href="/">
+                    <button className={styles.backButton}>Back to Home</button>
+                </Link>
+            </div>
+            
         </div>
         )
 }
