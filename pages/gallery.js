dd612a31c2cd708ca40f1cee7e8875086a9a5d12
Mateo Sam <sammateo4@gmail.com>
2021-03-27 22:28:51 -0400

    Started Gallery Page

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,11 +1,26 @@
 import Link from 'next/link'
+import styles from '../styles/gallery.module.css'
+import Image from 'next/image'
 export default function Gallery(){
-    return(
-        <div>
-            <h1>Hi</h1>
 
+    let imgarray = ["/../public/logo.jpeg","/../public/roti.jpg"]
+    return(
+        <div className={styles.container}>
+            <h1>Gallery</h1>
+            <div className={styles.gallery}>
+            {imgarray.map((img,id)=>(
+                    <div className={styles.logo}>
+                    <Image
+                      src={img}
+                      alt="logo"
+                      width={300}
+                      height={300}
+                      />
+                </div>
+            ))}
+            </div>
             <Link href="/">
-                <button>Back to Home</button>
+                <button className={styles.backButton}>Back to Home</button>
             </Link>
         </div>
         )
