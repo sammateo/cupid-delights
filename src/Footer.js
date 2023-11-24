3c4e9086cef4bb87755043f39b21a308d5c1ba4e
Mateo Sam <msam@the_port.com>
2023-11-24 15:07:48 -0400

    min order is 6

--- src/Footer.js
+++ src/Footer.js
@@ -1,36 +1,37 @@
 import styles from "../styles/Footer.module.css";
 import Link from "next/link";
 export default function Footer() {
-	return (
-		<div className={styles.footer}>
-			<div>
-				<h3>Contact Information</h3>
-				<p>Call/Whatsapp</p>
-				{/* <span><a href="tel:+44 7432 577502">+44 7432 577502</a></span> */}
-				<span>
-					<a href="https://wa.me/447432577502">+44 7432 577502</a>
-				</span>
-				<p>Email</p>
-				<p>
-					<a href="mailto:cupidelights@gmail.com"> cupidelights@gmail.com</a>
-				</p>
-				{/* <p>Whatsapp +44 7432 577502</p> */}
-				<Link href="https://www.facebook.com/Cupid-Delights-103544264841798/?tn-str=k*F">
-					<a target="_blank">Facebook</a>
-				</Link>
-				<br></br>
-				<Link href="https://www.instagram.com/cupidelights/?hl=en">
-					<a target="_blank">
-						<img src="/ig.png" width={20} height={20}></img>
-					</a>
-				</Link>
-			</div>
+  return (
+    <div className={styles.footer}>
+      <div>
+        <h3>Contact Information</h3>
+        <p>Call/Whatsapp</p>
+        {/* <span><a href="tel:+44 7432 577502">+44 7432 577502</a></span> */}
+        <span>
+          <a href="https://wa.me/447432577502">+44 7432 577502</a>
+        </span>
+        <p>Email</p>
+        <p>
+          <a href="mailto:cupidelights@gmail.com"> cupidelights@gmail.com</a>
+        </p>
+        {/* <p>Whatsapp +44 7432 577502</p> */}
+        <a
+          href="https://www.facebook.com/Cupid-Delights-103544264841798/?tn-str=k*F"
+          target="_blank"
+        >
+          Facebook
+        </a>
+        <br></br>
+        <a href="https://www.instagram.com/cupidelights/?hl=en" target="_blank">
+          <img src="/ig.png" width={20} height={20}></img>
+        </a>
+      </div>
 
-			<p>
-				Cupid Delights aims to satisfy your taste buds &amp; give you a taste of
-				"home" with our delicious roti skins delivered straight to your door via
-				our swift postal service.
-			</p>
-		</div>
-	);
+      <p>
+        Cupid Delights aims to satisfy your taste buds &amp; give you a taste of
+        "home" with our delicious roti skins delivered straight to your door via
+        our swift postal service.
+      </p>
+    </div>
+  );
 }
--- src/Options.js
+++ src/Options.js
@@ -1,267 +1,223 @@
 import { useState } from "react";
 import styles from "../styles/Options.module.css";
 export default function Options({
-	// checkout,
-	name1,
-	name2,
-	name3,
-	name4,
-	name5,
-	name6,
-	option1,
-	option2,
-	option3,
-	option4,
-	option5,
-	option6,
-	price1,
-	price2,
-	price3,
-	price4,
-	price5,
-	price6,
-	total,
-	total1,
-	total2,
-	total3,
-	total4,
-	total5,
-	total6,
-	// shipping1,
-	// shipping2,
-	// shipping3,
-	// nextshipping1,
-	// nextshipping2,
-	// nextshipping3,
-	// shipping,
-	// nextDay,
-	quantity,
-	setCheckout,
-	setOption1,
-	setOption2,
-	setOption3,
-	setOption4,
-	setOption5,
-	setOption6,
-	// setPrice1,
-	// setPrice2,
-	// setPrice3,
-	setTotal,
-	setTotal1,
-	setTotal2,
-	setTotal3,
-	setTotal4,
-	setTotal5,
-	setTotal6,
-	// setShipping1,
-	// setShipping2,
-	// setShipping3,
-	// setNextShipping1,
-	// setNextShipping2,
-	// setNextShipping3,
-	// setShipping,
-	// setNextDay,
-	setQuantity,
+  // checkout,
+  name1,
+  name2,
+  name3,
+  name4,
+  name5,
+  name6,
+  option1,
+  option2,
+  option3,
+  option4,
+  option5,
+  option6,
+  price1,
+  price2,
+  price3,
+  price4,
+  price5,
+  price6,
+  total,
+  total1,
+  total2,
+  total3,
+  total4,
+  total5,
+  total6,
+  quantity,
+  setCheckout,
+  setOption1,
+  setOption2,
+  setOption3,
+  setOption4,
+  setOption5,
+  setOption6,
+  setTotal,
+  setTotal1,
+  setTotal2,
+  setTotal3,
+  setTotal4,
+  setTotal5,
+  setTotal6,
+  setQuantity,
 }) {
-	// const [checkout, setCheckout] = useState(false);
-	// let option1 =0;
-	var formatter = new Intl.NumberFormat("en-IN", {
-		style: "currency",
-		currency: "GBP",
-	});
+  var formatter = new Intl.NumberFormat("en-IN", {
+    style: "currency",
+    currency: "GBP",
+  });
+  const quantityMultiplier = 6;
+  let updatedVal;
+  function add(event) {
+    setCheckout(false);
+    if (event.target.name == "option1") {
+      updatedVal = Number(event.target.value) + quantityMultiplier;
+      event.target.value = updatedVal;
+      setOption1(event.target.value);
+      setTotal1(total1 + price1 * quantityMultiplier);
+      setTotal(total + price1 * quantityMultiplier);
+      setQuantity(quantity + 1 * quantityMultiplier);
+    }
 
-	function add(event) {
-		setCheckout(false);
-		if (event.target.name == "option1") {
-			event.target.value++;
-			setOption1(event.target.value);
-			setTotal1(total1 + price1);
-			setTotal(total + price1);
-			setQuantity(quantity + 1);
-		}
+    if (event.target.name == "option2") {
+      updatedVal = Number(event.target.value) + quantityMultiplier;
+      event.target.value = updatedVal;
+      setOption2(event.target.value);
+      setTotal2(total2 + price2 * quantityMultiplier);
+      setTotal(total + price2 * quantityMultiplier);
+      setQuantity(quantity + 1 * quantityMultiplier);
+    }
+    if (event.target.name == "option3") {
+      updatedVal = Number(event.target.value) + quantityMultiplier;
+      event.target.value = updatedVal;
+      setOption3(event.target.value);
+      setTotal3(total3 + price3 * quantityMultiplier);
+      setTotal(total + price3 * quantityMultiplier);
+      setQuantity(quantity + 1 * quantityMultiplier);
+    }
+    if (event.target.name == "option4") {
+      updatedVal = Number(event.target.value) + quantityMultiplier;
+      event.target.value = updatedVal;
+      setOption4(event.target.value);
+      setTotal4(total4 + price4 * quantityMultiplier);
+      setTotal(total + price4 * quantityMultiplier);
+      setQuantity(quantity + 1 * quantityMultiplier);
+    }
+    if (event.target.name == "option5") {
+      updatedVal = Number(event.target.value) + quantityMultiplier;
+      event.target.value = updatedVal;
+      setOption5(event.target.value);
+      setTotal5(total5 + price5 * quantityMultiplier);
+      setTotal(total + price5 * quantityMultiplier);
+      setQuantity(quantity + 1 * quantityMultiplier);
+    }
+    if (event.target.name == "option6") {
+      updatedVal = Number(event.target.value) + quantityMultiplier;
+      event.target.value = updatedVal;
+      setOption6(event.target.value);
+      setTotal6(total6 + price6 * quantityMultiplier);
+      setTotal(total + price6 * quantityMultiplier);
+      setQuantity(quantity + 1 * quantityMultiplier);
+    }
+  }
 
-		if (event.target.name == "option2") {
-			event.target.value++;
-			setOption2(event.target.value);
-			setTotal2(total2 + price2);
-			setTotal(total + price2);
-			setQuantity(quantity + 1);
-		}
-		if (event.target.name == "option3") {
-			event.target.value++;
-			setOption3(event.target.value);
-			setTotal3(total3 + price3);
-			setTotal(total + price3);
-			setQuantity(quantity + 1);
-		}
-		if (event.target.name == "option4") {
-			event.target.value++;
-			setOption4(event.target.value);
-			setTotal4(total4 + price4);
-			setTotal(total + price4);
-			setQuantity(quantity + 1);
-		}
-		if (event.target.name == "option5") {
-			event.target.value++;
-			setOption5(event.target.value);
-			setTotal5(total5 + price5);
-			setTotal(total + price5);
-			setQuantity(quantity + 1);
-		}
-		if (event.target.name == "option6") {
-			event.target.value++;
-			setOption6(event.target.value);
-			setTotal6(total6 + price6);
-			setTotal(total + price6);
-			setQuantity(quantity + 1);
-		}
-	}
+  function minus(event) {
+    setCheckout(false);
+    if (event.target.name == "option1" && option1 > 0) {
+      event.target.value = Number(event.target.value) - quantityMultiplier;
+      setOption1(event.target.value);
+      setTotal1(total1 - price1 * quantityMultiplier);
+      setTotal(total - price1 * quantityMultiplier);
+      setQuantity(quantity - 1 * quantityMultiplier);
+    }
 
-	function minus(event) {
-		setCheckout(false);
-		if (event.target.name == "option1" && option1 > 0) {
-			event.target.value--;
-			setOption1(event.target.value);
-			setTotal1(total1 - price1);
-			setTotal(total - price1);
-			setQuantity(quantity - 1);
-		}
+    if (event.target.name == "option2" && option2 > 0) {
+      event.target.value = Number(event.target.value) - quantityMultiplier;
+      setOption2(event.target.value);
+      setTotal2(total2 - price2 * quantityMultiplier);
+      setTotal(total - price2 * quantityMultiplier);
+      setQuantity(quantity - 1 * quantityMultiplier);
+    }
+    if (event.target.name == "option3" && option3 > 0) {
+      event.target.value = Number(event.target.value) - quantityMultiplier;
+      setOption3(event.target.value);
+      setTotal3(total3 - price3 * quantityMultiplier);
+      setTotal(total - price3 * quantityMultiplier);
+      setQuantity(quantity - 1 * quantityMultiplier);
+    }
+    if (event.target.name == "option4" && option4 > 0) {
+      event.target.value = Number(event.target.value) - quantityMultiplier;
+      setOption4(event.target.value);
+      setTotal4(total4 - price4 * quantityMultiplier);
+      setTotal(total - price4 * quantityMultiplier);
+      setQuantity(quantity - 1 * quantityMultiplier);
+    }
 
-		if (event.target.name == "option2" && option2 > 0) {
-			event.target.value--;
-			setOption2(event.target.value);
-			setTotal2(total2 - price2);
-			setTotal(total - price2);
-			setQuantity(quantity - 1);
-		}
-		if (event.target.name == "option3" && option3 > 0) {
-			event.target.value--;
-			setOption3(event.target.value);
-			setTotal3(total3 - price3);
-			setTotal(total - price3);
-			setQuantity(quantity - 1);
-		}
-		if (event.target.name == "option4" && option4 > 0) {
-			event.target.value--;
-			setOption4(event.target.value);
-			setTotal4(total4 - price4);
-			setTotal(total - price4);
-			setQuantity(quantity - 1);
-		}
+    if (event.target.name == "option5" && option5 > 0) {
+      event.target.value = Number(event.target.value) - quantityMultiplier;
+      setOption5(event.target.value);
+      setTotal5(total5 - price5 * quantityMultiplier);
+      setTotal(total - price5 * quantityMultiplier);
+      setQuantity(quantity - 1 * quantityMultiplier);
+    }
+    if (event.target.name == "option6" && option6 > 0) {
+      event.target.value = Number(event.target.value) - quantityMultiplier;
+      setOption6(event.target.value);
+      setTotal6(total5 - price6 * quantityMultiplier);
+      setTotal(total - price6 * quantityMultiplier);
+      setQuantity(quantity - 1 * quantityMultiplier);
+    }
+  }
 
-		if (event.target.name == "option5" && option5 > 0) {
-			event.target.value--;
-			setOption5(event.target.value);
-			setTotal5(total5 - price5);
-			setTotal(total - price5);
-			setQuantity(quantity - 1);
-		}
-		if (event.target.name == "option6" && option6 > 0) {
-			event.target.value--;
-			setOption6(event.target.value);
-			setTotal6(total5 - price6);
-			setTotal(total - price6);
-			setQuantity(quantity - 1);
-		}
-	}
-
-	// function checkoutFunc() {
-	// 	if (quantity < 6) {
-	// 		alert("Minimum order is 6 roti skins");
-	// 		setCheckout(false);
-	// 	}
-	// 	if (quantity >= 6 && quantity <= 12) {
-	// 		nextDay ? setShipping(nextshipping1) : setShipping(shipping1);
-	// 		setCheckout(true);
-	// 	}
-	// 	if (quantity >= 13 && quantity <= 19) {
-	// 		nextDay ? setShipping(nextshipping2) : setShipping(shipping2);
-	// 		setCheckout(true);
-	// 	}
-	// 	if (quantity >= 20 && quantity <= 40) {
-	// 		nextDay ? setShipping(nextshipping3) : setShipping(shipping3);
-	// 		setCheckout(true);
-	// 	}
-
-	// 	if (quantity > 40) {
-	// 		alert("Contact us for orders larger than 40 roti skins");
-	// 		setCheckout(true);
-	// 	}
-	// }
-
-	// function changeShipType(event) {
-	// 	setCheckout(false);
-	// 	event.target.id == "nextDay" ? setNextDay(true) : setNextDay(false);
-	// 	// console.log(nextDay)
-	// }
-
-	return (
-		<div className={styles.options}>
-			<span>{formatter.format(price1)}</span>
-			<p>{name1}</p>{" "}
-			<button
-				name="option1"
-				value={option1}
-				onClick={add}
-				className="plusbutton"
-			>
-				{" "}
-				+{" "}
-			</button>{" "}
-			<button
-				name="option1"
-				onClick={minus}
-				value={option1}
-				className="minusbutton"
-			>
-				{" "}
-				-{" "}
-			</button>
-			<br></br>
-			<span>{formatter.format(price2)}</span>
-			<p>{name2}</p>{" "}
-			<button
-				value={option2}
-				name="option2"
-				onClick={add}
-				className="plusbutton"
-			>
-				{" "}
-				+{" "}
-			</button>{" "}
-			<button
-				name="option2"
-				onClick={minus}
-				value={option2}
-				className="minusbutton"
-			>
-				{" "}
-				-{" "}
-			</button>
-			{/* Buss Up Shot */}
-			<br></br>
-			<span>{formatter.format(price3)}</span>
-			<p>{name3}</p>{" "}
-			<button
-				value={option3}
-				name="option3"
-				onClick={add}
-				className="plusbutton"
-			>
-				{" "}
-				+{" "}
-			</button>{" "}
-			<button
-				name="option3"
-				onClick={minus}
-				value={option3}
-				className="minusbutton"
-			>
-				{" "}
-				-{" "}
-			</button>
-			{/* Mini Plain */}
-			{/* <br></br>
+  return (
+    <div className={styles.options}>
+      <span>{formatter.format(price1)}</span>
+      <p>{name1}</p>{" "}
+      <button
+        name="option1"
+        value={option1}
+        onClick={add}
+        className="plusbutton"
+      >
+        {" "}
+        +{" "}
+      </button>{" "}
+      <button
+        name="option1"
+        onClick={minus}
+        value={option1}
+        className="minusbutton"
+      >
+        {" "}
+        -{" "}
+      </button>
+      <br></br>
+      <span>{formatter.format(price2)}</span>
+      <p>{name2}</p>{" "}
+      <button
+        value={option2}
+        name="option2"
+        onClick={add}
+        className="plusbutton"
+      >
+        {" "}
+        +{" "}
+      </button>{" "}
+      <button
+        name="option2"
+        onClick={minus}
+        value={option2}
+        className="minusbutton"
+      >
+        {" "}
+        -{" "}
+      </button>
+      {/* Buss Up Shot */}
+      <br></br>
+      <span>{formatter.format(price3)}</span>
+      <p>{name3}</p>{" "}
+      <button
+        value={option3}
+        name="option3"
+        onClick={add}
+        className="plusbutton"
+      >
+        {" "}
+        +{" "}
+      </button>{" "}
+      <button
+        name="option3"
+        onClick={minus}
+        value={option3}
+        className="minusbutton"
+      >
+        {" "}
+        -{" "}
+      </button>
+      {/* Mini Plain */}
+      {/* <br></br>
 			<span>{formatter.format(price4)}</span>
 			<p>{name4}</p>{" "}
 			<button
@@ -282,8 +238,8 @@ export default function Options({
 				{" "}
 				-{" "}
 			</button> */}
-			{/* Mini Dhal */}
-			{/* <br></br>
+      {/* Mini Dhal */}
+      {/* <br></br>
 			<span>{formatter.format(price5)}</span>
 			<p>{name5}</p>{" "}
 			<button
@@ -304,8 +260,8 @@ export default function Options({
 				{" "}
 				-{" "}
 			</button> */}
-			{/* Mini Buss Up shot */}
-			{/* <br></br>
+      {/* Mini Buss Up shot */}
+      {/* <br></br>
 			<span>{formatter.format(price6)}</span>
 			<p>{name6}</p>{" "}
 			<button
@@ -326,6 +282,6 @@ export default function Options({
 				{" "}
 				-{" "}
 			</button> */}
-		</div>
-	);
+    </div>
+  );
 }
